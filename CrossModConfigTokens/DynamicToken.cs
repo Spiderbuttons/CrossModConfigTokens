516d110b4b2ab4e19d18b1a2a2b1ac49b2ae8ec3
--unset <azurylldmm@gmail.com>
2024-10-01 01:32:52 -0400

    Cleaned up DynamicToken class.

--- CrossModConfigTokens/DynamicToken.cs
+++ CrossModConfigTokens/DynamicToken.cs
@@ -3,36 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using CrossModConfigTokens.Helpers;
 using HarmonyLib;
-using Newtonsoft.Json.Linq;
-using StardewModdingAPI;
-using StardewValley;
 
 namespace CrossModConfigTokens
 {
-    /// <summary>Method delegates which represent a simplified version of <see cref="IValueProvider"/> that can be implemented by custom mod tokens through the API via <see cref="ConventionValueProvider"/>.</summary>
-    /// <remarks>Methods should be kept in sync with <see cref="ConventionWrapper"/>.</remarks>
     internal class DynamicToken
     {
-        /*********
-         ** Fields
-         *********/
         private object? dynamicTokenManager;
 
         private readonly object emptyInputArgs =
             Activator.CreateInstance(AccessTools.TypeByName("ContentPatcher.Framework.Tokens.EmptyInputArguments"),
                 new object[] { })!;
-
-        private string? uniqueID;
-        private string dynamicTokenKey = string.Empty;
-        private List<string> dynamicTokenValue = [];
-
-        private bool refresh = false;
-
-        /****
-         ** Metadata
-         ****/
+        
+        
         /// <summary>Get whether the token allows input arguments (e.g. an NPC name for a relationship token).</summary>
         /// <remarks>Default false.</remarks>
         public bool AllowsInput()
@@ -52,7 +35,7 @@ namespace CrossModConfigTokens
         /// <remarks>Default true.</remarks>
         public bool CanHaveMultipleValues(string? input = null)
         {
-            return false;
+            return true;
         }
 
         /// <summary>Validate that the provided input arguments are valid.</summary>
@@ -87,34 +70,7 @@ namespace CrossModConfigTokens
         /// <returns>Returns whether the value changed, which may trigger patch updates.</returns>
         public bool UpdateContext()
         {
-            return true;
-
-            // if (dynamicTokenManager is null)
-            // {
-            //     dynamicTokenManager = ModEntry.GrabDynamicToken(uniqueID, dynamicTokenKey);
-            // }
-            //
-            // if (dynamicTokenManager is null) return true;
-            //
-            // var oldValue = new List<string>(dynamicTokenValue);
-            //
-            // var values = AccessTools.Method(dynamicTokenManager.GetType(), "GetValues")
-            //     .Invoke(dynamicTokenManager, new object[] { emptyInputArgs });
-            // if (values is null)
-            // {
-            //     Log.Alert("No values! Returning true.");
-            //     return true;
-            // }
-            //
-            // dynamicTokenValue.Clear();
-            // foreach (var value in (values as IEnumerable<string>)!)
-            // {
-            //     dynamicTokenValue.Add(value);
-            // }
-            //
-            // var shouldUpdate = !oldValue.SequenceEqual(dynamicTokenValue);
-            // Log.Alert($"Should update: {shouldUpdate}.");
-            // return shouldUpdate;
+            return true; // I'm sorry. It's the only way.
         }
 
         /// <summary>Get whether the token is available for use.</summary>
@@ -129,14 +85,14 @@ namespace CrossModConfigTokens
         public IEnumerable<string> GetValues(string? input)
         {
             if (input is null) yield break;
-            var split = input?.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(p => p.Trim()).ToArray() ?? [];
+            var split = input.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(p => p.Trim()).ToArray();
             if (split.Length != 2)
             {
                 yield break;
             }
 
-            uniqueID = split[0];
-            dynamicTokenKey = split[1];
+            var uniqueID = split[0];
+            var dynamicTokenKey = split[1];
 
             dynamicTokenManager = ModEntry.GrabDynamicToken(uniqueID, dynamicTokenKey);
             if (dynamicTokenManager is null) yield break;
