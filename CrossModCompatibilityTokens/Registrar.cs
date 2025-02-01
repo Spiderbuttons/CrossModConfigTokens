be027f81d6272c8c7f8d99dc5125a417052e1ade
Spiderbuttons <azurylldmm@gmail.com>
2025-01-31 22:14:42 -0500

    Added Registrar to handle grabbing and shuffling around mods and content packs.

---
+++ CrossModCompatibilityTokens/Registrar.cs
@@ -0,0 +1,107 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Security.AccessControl;
+using CrossModCompatibilityTokens.Helpers;
+using Newtonsoft.Json.Linq;
+using StardewModdingAPI;
+using StardewModdingAPI.Framework;
+using StardewModdingAPI.Framework.ModHelpers;
+
+namespace CrossModCompatibilityTokens;
+
+public static class Registrar
+{
+    public static ModRegistry Registry { get; set; }
+
+    static Registrar()
+    {
+        Registry = StardewModdingAPI.Framework.SCore.Instance.ModRegistry;
+    }
+
+    public static bool AreAllModsLoaded()
+    {
+        return Registry.AreAllModsLoaded;
+    }
+    
+    public static bool TryGetModMetadata(string id, [NotNullWhen(true)] out IModMetadata? mod, out string? error)
+    {
+        mod = null;
+        error = null;
+        if (!AreAllModsLoaded())
+        {
+            error = "SMAPI has not finished loading mods yet!";
+            return false;
+        }
+        if (Registry.Get(id) is null)
+        {
+            error = $"{id} does not exist in SMAPI's mod registry!";
+            return false;
+        }
+        mod = Registry.Get(id);
+        return true;
+    }
+
+    public static bool TryGetMod(string id, [NotNullWhen(true)] out IMod? mod, out string? error)
+    {
+        mod = null;
+        error = null;
+        if (!TryGetModMetadata(id, out var metadata, out error))
+        {
+            return false;
+        }
+
+        if (!metadata.IsContentPack)
+        {
+            mod = metadata.Mod!;
+            return true;
+        }
+        
+        error = $"{id} is a content pack.";
+        return false;
+    }
+
+    public static bool TryGetContentPack(string id, [NotNullWhen(true)] out IContentPack? pack, out string? error)
+    {
+        pack = null;
+        error = null;
+        if (!TryGetModMetadata(id, out var metadata, out error))
+        {
+            return false;
+        }
+        
+        if (metadata.IsContentPack)
+        {
+            pack = metadata.ContentPack!;
+            return true;
+        }
+        
+        error = $"{id} is not a content pack!";
+        return false;
+    }
+
+    public static bool TryGetModHelper(string id, [NotNullWhen(true)] out IModHelper? helper, out string? error)
+    {
+        helper = null;
+        error = null;
+        if (!TryGetMod(id, out var mod, out error))
+        {
+            return false;
+        }
+
+        helper = mod!.Helper;
+        return true;
+    }
+
+    public static bool TryGetModAssembly(string id, [NotNullWhen(true)] out Assembly? assembly, out string? error)
+    {
+        assembly = null;
+        error = null;
+        if (!TryGetMod(id, out var mod, out error))
+        {
+            return false;
+        }
+
+        assembly = mod!.GetType().Assembly;
+        return true;
+    }
+}
