f996f05027d3700da385782e6d92309523485b4b
Spiderbuttons <azurylldmm@gmail.com>
2025-02-01 02:10:03 -0500

    Made my logging utilities null safe.

--- CrossModCompatibilityTokens/Helpers/Log.cs
+++ CrossModCompatibilityTokens/Helpers/Log.cs
@@ -8,27 +8,27 @@ namespace CrossModCompatibilityTokens.Helpers;
 public static class Log
 {
     public static void Debug<T>(T message) => ModEntry.ModMonitor.Log(
-        $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
+        $"{(message is not string ? "[" + message?.GetType() + "] " : string.Empty)}{message?.ToString() ?? string.Empty}",
         LogLevel.Debug);
 
     public static void Error<T>(T message) => ModEntry.ModMonitor.Log(
-        $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
+        $"{(message is not string ? "[" + message?.GetType() + "] " : string.Empty)}{message?.ToString() ?? string.Empty}",
         LogLevel.Error);
 
     public static void Warn<T>(T message) => ModEntry.ModMonitor.Log(
-        $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
+        $"{(message is not string ? "[" + message?.GetType() + "] " : string.Empty)}{message?.ToString() ?? string.Empty}",
         LogLevel.Warn);
 
     public static void Info<T>(T message) => ModEntry.ModMonitor.Log(
-        $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
+        $"{(message is not string ? "[" + message?.GetType() + "] " : string.Empty)}{message?.ToString() ?? string.Empty}",
         LogLevel.Info);
 
     public static void Trace<T>(T message) => ModEntry.ModMonitor.Log(
-        $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
+        $"{(message is not string ? "[" + message?.GetType() + "] " : string.Empty)}{message?.ToString() ?? string.Empty}",
         LogLevel.Trace);
 
     public static void Alert<T>(T message) => ModEntry.ModMonitor.Log(
-        $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
+        $"{(message is not string ? "[" + message?.GetType() + "] " : string.Empty)}{message?.ToString() ?? string.Empty}",
         LogLevel.Alert);
 
     public static void ILCode(IEnumerable<CodeInstruction> newCode, IEnumerable<CodeInstruction> originalCode)
